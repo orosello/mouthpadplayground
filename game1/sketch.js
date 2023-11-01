5d1d2b967df4ce13cdb2e6c038d9a4a08baf2d4c
Oscar Rosello <oscar@augmental.tech>
2023-10-31 17:32:00 -0700

    added text to sketch 1

--- game1/sketch.js
+++ game1/sketch.js
@@ -18,5 +18,38 @@ function draw() {
   stroke(255); // Set the stroke color to white
   rectMode(CENTER);
   rect(300, 200, width - 10, height - 10); // Draw a rectangle around the canvas
-}
 
+  if (dist(mouseX, mouseY, width, 0) < 50) {
+    fill(255);
+    noStroke;
+    textFont("Inter");
+    textSize(20);
+    text("Top right ☑", 480, 30);
+  }
+  if (dist(mouseX, mouseY, 0, height) < 50) {
+    fill(255);
+    noStroke;
+    textFont("Inter");
+    textSize(20);
+    text("Bottom left ☑", 10, 380);
+  }
+  if (dist(mouseX, mouseY, width, height) < 50) {
+    fill(255);
+    noStroke;
+    textFont("Inter");
+    textSize(20);
+    text("Bottom right ☑", 450, 380);
+  }
+  if (dist(mouseX, mouseY, 0, 0) < 50) {
+    fill(255);
+    noStroke;
+    textFont("Inter");
+    textSize(20);
+    text("Top left ☑", 10, 30);
+  }
+
+}
+  
+
+
+  

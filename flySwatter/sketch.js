f806fae1606ff7f306a30d9ba39264ec7fde1472
Oscar Rosello <rosello@Oscars-MBP.MG8702>
2023-12-13 16:11:19 -0800

    modified fly swatter

--- flySwatter/sketch.js
+++ flySwatter/sketch.js
@@ -1,33 +1,95 @@
-let circleRadius = 40;
+let circleRadius = 20;
+let b;
+let bubbles = [];
+let state = "bubbles"; // Add a state variable
+let stateChangeTime;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < 20; i++) {
+    let x = windowWidth / 2 + random(-100, 100);
+    let y = windowHeight / 2 + random(-100, 100);
+    let r = circleRadius;
+    b = new Bubble(x, y, r);
+    bubbles.push(b);
+  }
 }
 
 function draw() {
-  background(0);
+  if (state === "bubbles") {
+    background(0);
+
+    for (let i = 0; i < bubbles.length; i++) {
+      bubbles[i].move();
+      bubbles[i].show();
+    }
+
+    if (bubbles.length === 0) {
+      state = "animation";
+      stateChangeTime = millis(); // Record the time when the state changes
+    }
+  } else if (state === "animation") {
+    background("black");
+    let gap = calculateSpacing(10);
+    for (let i = 0; i <= width; i += gap) {
+      for (let j = 0; j <= height; j += 50) {
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        noStroke();
+        circle(i, j, 50);
+      }
+    }
+
+    if (millis() - stateChangeTime > 3000) {
+      // If 3 seconds have passed
+      state = "black";
+    }
+  } else if (state === "black") {
+    background(0);
+  }
+}
+
+function mousePressed() {
+  for (let i = bubbles.length - 1; i >= 0; i--) {
+    if (bubbles[i].onHover()) {
+      bubbles.splice(i, 1);
+    }
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-class Bubble{
+function calculateSpacing(ballNumber) {
+  return width / ballNumber;
+}
 
-  constructor(x, y, r){
+class Bubble {
+  constructor(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
   }
 
-  show(){
-    fill
-    circle(this.x, this.y, this.r);
-
+  show() {
+    if (this.onHover()) {
+      fill(0);
+      stroke(255);
+    } else {
+      fill(255);
+      noStroke();
+    }
+    circle(this.x, this.y, this.r * 2);
   }
 
+  move() {
+    this.x = this.x + random(-4, 4);
+    this.y = this.y + random(-4, 4);
+  }
 
-
+  onHover() {
+    let d = dist(mouseX, mouseY, this.x, this.y);
+    return d < this.r;
+  }
 }
-
-

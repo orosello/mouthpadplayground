4e97d3639aa86eb34f216cbee6eab03aafd7d304
orosello <osc.rosello@gmail.com>
2023-12-20 21:57:22 -0800

    [paint] fixed brush radius

--- paint/sketch.js
+++ paint/sketch.js
@@ -3,13 +3,13 @@ let randomCircleRadius = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(RGB);
-  print("hello");
   background(0);
 }
 
 function draw() {
   if (mouseIsPressed) {
-    randomCircleRadius = random(10, 80);
+    // randomCircleRadius = random(10, 80);
+    randomCircleRadius = 70;
 
     fill(255);
     strokeWeight(1);

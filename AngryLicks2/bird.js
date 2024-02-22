2dddb00d4642a8c7abc6a311eb8ed49eafc50746
orosello <osc.rosello@gmail.com>
2024-02-21 17:40:45 -0800

    Angry Licks 2 base game

--- AngryLicks2/bird.js
+++ AngryLicks2/bird.js
@@ -1,12 +1,23 @@
 class Bird {
   constructor(x, y, r) {
-    this.x = x;
-    this.y = y;
+    const options = {
+      restitution: 0.5,
+    };
+    this.body = Matter.Bodies.circle(x, y, r, options);
+    Matter.Body.setMass(this.body, this.body.mass * 4);
+    Matter.World.add(world, this.body);
     this.r = r;
   }
 
   show() {
-    fill(255);
-    circle(this.x, this.y, this.r);
+    const pos = this.body.position;
+    const angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    // imageMode(CENTER);
+    // image(dotImg, 0, 0, this.r * 2, this.r * 2);
+    circle(0, 0, this.r * 2);
+    pop();
   }
 }
--- AngryLicks2/ground.js
+++ AngryLicks2/ground.js
@@ -1,12 +1,7 @@
-class Box {
+class Ground extends Box {
   constructor(x, y, w, h) {
-    const options = {
-      restitution: 0.5,
-    };
-    this.body = Matter.Bodies.rectangle(x, y, w, h, options);
-    Matter.World.add(world, this.body);
-    this.w = w;
-    this.h = h;
+    super(x, y, w, h);
+    this.body.isStatic = true;
   }
 
   show() {
@@ -19,8 +14,6 @@ class Box {
     fill(255);
     rectMode(CENTER);
     rect(0, 0, this.w, this.h);
-    // imageMode(CENTER);
-    // image(boxImg, 0, 0, this.w, this.h);
     pop();
   }
 }

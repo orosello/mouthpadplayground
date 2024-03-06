7c57713cf5bff4ec02fc8d7d7d8d15d36d79a5e4
orosello <osc.rosello@gmail.com>
2024-03-06 09:57:59 -0800

    Basic navigation bar

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const gameOrder = [
+  "/edgeNavigation/edgeNavigation.html",
+  "/chaseBall/chaseBall.html",
+  "/OnOff/onOff.html",
+  "/fitts/fitts.html",
+  "/chaseBallRightClick/chaseBallRightClick.html",
+  "/chaseBallDoubleClick/chaseBallDoubleClick.html",
+  "/orbit/orbit.html",
+  "/dragHole/dragHole.html",
+  "/paint/paint.html",
+];
+
+function getCurrentGameIndex() {
+  const currentGame = window.location.pathname.split("/").pop();
+  return gameOrder.findIndex((game) => game.includes(currentGame));
+}
+
+function goToPreviousGame() {
+  const currentIndex = getCurrentGameIndex();
+  if (currentIndex > 0) {
+    window.location.href = gameOrder[currentIndex - 1];
+  }
+}
+
+function goToNextGame() {
+  const currentIndex = getCurrentGameIndex();
+  if (currentIndex < gameOrder.length - 1) {
+    window.location.href = gameOrder[currentIndex + 1];
+  }
+}
+
+function reloadGame() {
+  window.location.reload();
+}
+
+function goToMenu() {
+  window.location.href = "../index.html"; // Adjust the path as necessary
+}

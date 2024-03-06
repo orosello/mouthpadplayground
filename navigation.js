96ef3c58380c3b909b7e2adafff5235a2d690f34
orosello <osc.rosello@gmail.com>
2024-03-06 15:07:26 -0800

    [NavBar] Updated all pages, navbar basic functionality

---
+++ navigation.js
@@ -0,0 +1,9 @@
+document.write(`
+    <!-- Navigation Bar -->
+    <script src="../scripts.js"></script>
+    <button class="button" onclick="goToPreviousGame()">Previous Game</button>
+    <button class="button" onclick="goToNextGame()">Next Game</button>
+    <button class="button" onclick="reloadGame()">Reload</button>
+    <button class="button" onclick="goToMenu()">Menu</button>
+
+`);
--- paint/sketch.js
+++ paint/sketch.js
@@ -1,4 +1,13 @@
+const BOTTOM_TEXT_Y_OFFSET = 50;
+const FONT_SIZE = 16;
+
 let randomCircleRadius = 0;
+let myFont;
+let showInstructions = true; // This should ensure instructions are shown initially
+
+function preload() {
+  myFont = loadFont("../assets/Press_Start_2P/PressStart2P-Regular.ttf");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,18 +19,39 @@ function setup() {
   canvas.oncontextmenu = function (e) {
     e.preventDefault();
   };
+
+  // Event listener for mouse click to hide instructions
+  canvas.addEventListener("mousedown", function () {
+    showInstructions = false;
+  });
 }
 
 function draw() {
+  if (showInstructions) {
+    // Set text properties
+    textFont(myFont);
+    textSize(FONT_SIZE);
+    textAlign(CENTER);
+    fill(255);
+
+    // Display instructions
+    text(
+      "Click and drag to paint!",
+      windowWidth / 2,
+      windowHeight - BOTTOM_TEXT_Y_OFFSET
+    );
+  }
+
   if (mouseIsPressed) {
-    // randomCircleRadius = random(20, 80);
     randomCircleRadius = 70;
 
     fill(255);
     strokeWeight(1);
     rectMode(CENTER);
-    circle(mouseX, mouseY, randomCircleRadius, randomCircleRadius);
+    circle(mouseX, mouseY, randomCircleRadius);
   }
+
+  // Display instructions if the flag is true
 }
 
 function windowResized() {
